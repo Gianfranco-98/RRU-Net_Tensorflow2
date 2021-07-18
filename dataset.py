90ab77ca6224a1d87fe113ab64eec91862ff167e
Gianfranco Di Marco <71604276+Gianfranco-98@users.noreply.github.com>
2021-07-18 10:01:59 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -13,9 +13,10 @@ import os
 
 class Forgery_Detection_Dataset:
 
-    def __init__(self):
+    def __init__(self, ids):
         
         self.name = DATASET_NAME
+        self.ids = ids
 
         # Dataset directory setting
         self.dataset_dir = os.path.join(DATA_DIR, self.name)
@@ -24,14 +25,17 @@ class Forgery_Detection_Dataset:
         self.ground_truth_dir = os.path.join(self.dataset_dir, GROUND_TRUTH_FOLDER)
 
         # Dataset info setting
-        self.files = os.listdir(self.pristine_dir)
-        self.ids = [f[:-4] for f in self.files]
+        """self.ids = [f[:-4] for f in self.files]
+        random.shuffle(self.ids)
         train_indices = int(TRAIN_PERCENT * len(self.ids))
         val_indices = train_indices + int(VAL_PERCENT * len(self.ids))
         test_indices = val_indices + int(VAL_PERCENT * len(self.ids))
         self.train_ids = self.ids[:train_indices]
         self.val_ids = self.ids[train_indices:val_indices]
         self.test_ids = self.ids[val_indices:test_indices]
+        self.train_ids = self.ids[:TRAIN_SIZE]
+        self.val_ids = self.ids[TRAIN_SIZE:TRAIN_SIZE+VAL_SIZE]
+        self.test_ids = self.ids[TRAIN_SIZE+VAL_SIZE:]"""
 
     def __len__(self):
 
@@ -39,36 +43,38 @@ class Forgery_Detection_Dataset:
         
     def get_image(self, img_id, category=None):
 
+        suffix = '.jpg'
         if category == "pristine" or category is None:
             directory = self.pristine_dir
         elif category == "forgery":
             directory = self.forgered_dir
         elif category == "ground_truth":
             directory = self.ground_truth_dir
+            suffix = '_gt.png'
         else:
             raise ValueError("Wrong category type")
 
-        filename = [f for f in self.files if img_id in f][0]
+        filename = img_id + suffix
         image = cv2.cvtColor(cv2.imread(os.path.join(directory, filename)), cv2.COLOR_BGR2RGB)
         return image
     
     def generate(self, mode):
 
-        if mode == "train":
+        """if mode == "train":
             ids = self.train_ids
         elif mode == "val":
             ids = self.val_ids
         elif mode == "test":
             ids = self.test_ids
         elif mode == "example":
-            ids = self.ids
+            ids = self.ids"""
 
-        for img_id in ids:
+        for img_id in self.ids:
 
             # Get images
-            if mode == "example":
+            """if mode == "example":
                 pristine_img = self.get_image(img_id, "pristine") / 255.
-                pristine_img = cv2.resize(pristine_img, IMG_SHAPE) 
+                pristine_img = cv2.resize(pristine_img, IMG_SHAPE)"""
             forgery_img = self.get_image(img_id, "forgery") / 255.
             gt_img = self.get_image(img_id, "ground_truth") / 255.
 
@@ -77,7 +83,8 @@ class Forgery_Detection_Dataset:
                 forgery_img = cv2.resize(forgery_img, IMG_SHAPE)
                 gt_img = cv2.resize(gt_img, IMG_SHAPE)
 
-            if mode == "example":
+            yield forgery_img, gt_img
+            """if mode == "example":
                 yield forgery_img, gt_img, pristine_img
             else:
-                yield forgery_img, gt_img
+                yield forgery_img, gt_img"""
